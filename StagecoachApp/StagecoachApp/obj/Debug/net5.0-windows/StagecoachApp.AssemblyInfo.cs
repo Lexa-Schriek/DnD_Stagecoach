c9c4d5d2f0edfe4d59660ec1b38752f2dfb6cb60
Lexa <r0886330@student.thomasmore.be>
2023-12-17 18:45:03 +0100

    added some folders so the path on the publish action actually exists

--- StagecoachApp/StagecoachApp/obj/Debug/net5.0-windows/StagecoachApp.AssemblyInfo.cs
+++ StagecoachApp/StagecoachApp/obj/Debug/net5.0-windows/StagecoachApp.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("StagecoachApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Lexa Schriek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
